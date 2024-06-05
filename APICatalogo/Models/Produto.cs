15fc6a08344f82e44ef63e2028f26cd68f300db5
Thaysa Zooby <thaysa.zooby@gmail.com>
2024-06-05 17:14:13 -0300

    Inicial migrations

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,16 +1,32 @@
-﻿namespace APICatalogo.Models
-{
-    public class Produto
-    {
-        public int ProdutoId { get; set; }
-        public string? Nome { get; set; }
-        public string? Descricao { get; set; }
-        public decimal? Preco { get; set; }
-        public string? ImageUrl { get; set; }
-        public float Estoque { get; set; }
-        public DateTime DataCadastro { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-        public int CategoriaId { get; set; }
-        public Categoria? Categoria { get; set; }
-    }
+namespace APICatalogo.Models;
+
+[Table("Produtos")]
+public class Produto
+{
+    [Key]
+    public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(90)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(400)]
+    public string? Descricao { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal? Preco { get; set; }
+
+    [Required]
+    [StringLength(400)]
+    public string? ImageUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 }
